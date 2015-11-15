c9f80a252d12001242c135a317af66fece94d416
Caleb Evans <caleb@calebevans.me>
2015-11-14 16:27:54 -0800

    Add Gruntfile; automatically compile styles/scripts

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		pkg: grunt.file.readJSON('package.json'),
+
+		sass: {
+			options: {
+				style: 'compressed',
+				sourcemap: 'none',
+				cacheLocation: 'styles/sass/.sass-cache'
+			},
+			styles: {
+				files: {
+					'styles/css/admin.css': 'styles/sass/admin.scss',
+				}
+			}
+		},
+
+		postcss: {
+			options: {
+				map: false,
+				processors: [
+					require('autoprefixer')({
+						browsers: 'last 2 versions'
+					})
+				]
+			},
+			styles: {
+				src: 'styles/css/*.css'
+			}
+		},
+
+		uglify: {
+			options: {
+				sourceMap: false
+			},
+			scripts: {
+				files: {
+					'scripts/facebook.min.js': 'scripts/facebook.js',
+				}
+			}
+		},
+
+		watch: {
+			scripts: {
+				files: [
+					'scripts/facebook.js',
+				],
+				tasks: [
+					'uglify'
+				]
+			},
+			styles: {
+				files: [
+					'styles/sass/*.scss'
+				],
+				tasks: [
+					'sass',
+					'postcss'
+				]
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-sass');
+	grunt.loadNpmTasks('grunt-postcss');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+};
