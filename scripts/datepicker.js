7fd0eeb67abbb0e2b58e68e409c86d4f8112cf85
Caleb Evans <caleb@calebevans.me>
2016-09-19 21:34:13 -0700

    Restrict start date to current date or any future date

--- scripts/datepicker.js
+++ scripts/datepicker.js
@@ -2,7 +2,11 @@
 (function ($) {
 $(document).ready(function () {
 
-$('#sg_start_date').datepicker();
+$('#sg_start_date').datepicker({
+	// Restrict start date to current date or any date thereafter (a start date
+	// cannot be in the past)
+	minDate: 0
+});
 
 });
 }( window.jQuery ));
